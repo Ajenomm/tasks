3df30fa6f7f19dc8e39b1bf9b485b70587e1ba00
maana7900@gmail.com <maana7900@gmail.com>
2022-09-28 20:29:48 +0600

    add:addition

---
+++ script.js
@@ -0,0 +1,191 @@
+// 1
+// var arr = [1, 2, 3, 4, 5];
+// for ( var i = 0; i < arr.length; i++)
+// {
+//     document.write(arr[i]);
+// }
+
+
+
+
+// 2
+// var result = 1;
+// var arr = [1, 2, 3, 4, 5];
+// for ( var i = 0; i < arr.length; i++)
+// {
+//     result = result * arr[i];
+// }
+// alert(result);
+
+
+
+// 3
+// var obj = {
+// 	'Минск': 'Беларусь',
+// 	'Москва': 'Россия',
+// 	'Киев': 'Украина'
+// };
+// for (var key in obj) {
+//     alert(key + '-это' + obj[key] + '.');
+// }
+
+
+
+// 4
+// var i = 11;
+// while(i <= 33){
+//     document.write(i + '.')
+//     i++;
+// }
+
+
+
+// 5
+// var obj = {
+// 	'Коля': '200',
+// 	'Вася': '300',
+// 	'Петя': '400'
+// };
+// for (var key in obj) {
+//     alert(key + '- зарплата ' + obj[key] + '.');
+// }
+
+
+
+// 6
+
+
+
+// 7
+// var num = 34;
+// if (num == 10){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+
+// 8 
+// var min = 45;
+// if (min >= 0 && min <=14){
+//     alert('в первую');
+// }
+// if (min >=15 && min <= 30){
+//     alert('во вторую');
+// }
+// if (min >=31 && min <= 45){
+//     alert('в третью');
+// }
+// if (min >=46 && min <= 59){
+//     alert('В четвертую');
+// }
+
+
+
+
+// 9if
+// var lang = 'ru';
+// if (lang == 'ru'){
+//     var arr = ['пн', 'вт','ср','чт','сб','вс'];
+// }
+// if (lang == 'en'){
+//     var arr = ['mn', 'ts','wd','th','fr','sn'];
+// }
+// alert(arr);
+
+// 9swetch-case 
+// var lang = 'ru';
+// switch(lang){
+//     case 'ru':
+//         var arr = ['пн', 'вт','ср','чт','сб','вс'];
+//     break;
+//     case 'en':
+//         var arr = ['mn', 'ts','wd','th','fr','sn'];
+//     break;
+// }
+// alert(arr);
+
+// 9 многомерный массив
+
+
+
+// 10
+// var a = -3;
+// if (a = 0 ){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+// 11
+// var a = 1;
+// if (a < 0){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+// 12
+// var a = 1;
+// if (a > 0){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+
+// 13
+// var a = 1;
+// if (a <= 0){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+// 14
+// var a = 1;
+// if (a != 0){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+
+// 15
+// var a = 'test';
+// if (a == 'test'){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
+
+
+
+
+// 16
+// var a = '1';
+// if (a = '1'){
+//     alert ('верно')
+// } 
+// else{
+//     alert ('неверно')
+// }
